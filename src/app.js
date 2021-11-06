5d2a398696de40a89326c1050000d7aea9a516a5
Joao Paulo Duprat Pohlmann <duprat.dev@gmail.com>
2021-11-06 01:21:40 -0300

    Adicionada documentacao do projeto

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("./swagger.json");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 require("./routes/cursos")(app);
 require("./routes/alunos")(app);
